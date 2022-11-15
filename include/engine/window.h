eee406f839e09baf65e298954e8c6788144ef6ee
Justin Kachele <justin@kachele.com>
2022-11-15 01:16:24 -0500

    initial commit

---
+++ include/engine/window.h
@@ -0,0 +1,10 @@
+/*Project-------CPP_Game_Engine
+ *File----------window.h
+ *Author--------Justin Kachele
+ *Date----------11/14/2022
+ *License-------GNU GENERAL PUBLIC LICENSE
+******************************************/
+#ifndef window_H
+#define window_H
+
+#endif //window_H
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(__attribute__((unused)) GLFWwindow* window, int width, int height);
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600,
+                                          "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebuffer_size_callback(__attribute__((unused)) GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
